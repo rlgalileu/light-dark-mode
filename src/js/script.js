1245d2ccc8e06556958395f02e1cb7989932650c
Galileu Rodrigues de Lima <54282757+rlgalileu@users.noreply.github.com>
2022-03-03 23:34:18 -0300

    create dark light functions

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,50 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `./src/img/undraw_proud_coder_${color}.svg`;
+  image2.src = `./src/img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `./src/img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
+}
+
+// Dark Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'LightMode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
+}
 
 // Set Theme
 function setTheme(theme) {
-  document.documentElement.setAttribute('data-theme', theme)
+  document.documentElement.setAttribute('data-theme', theme);
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-  event.target.checked ? setTheme('dark') : setTheme('light');
+  if (event.target.checked) {
+    setTheme('dark');
+    darkMode();
+  } else {
+    setTheme('light')
+    lightMode();
+  }
 }
 
 // Event Listener
